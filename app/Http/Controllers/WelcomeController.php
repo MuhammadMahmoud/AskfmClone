9dcc1152f8406d30eb1609d057380f50f02177e3
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2021-06-07 01:39:43 +0200

    reimplement

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -12,58 +12,19 @@ use Session;
 use Hash;
 class WelcomeController extends Controller{
 
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+
 
     public function welcome()
     {
         # code...
         return view('welcome');
     }
-    public function show_login()
-    {
-        # code...
-        return view('login');
-    }
+
     public function show_home()
     {
         return view('homepage');
     }
-    public function login(Request $request)
-    {
-        // $data= DB::table('users')->where("email", $request->email);
-        // var_dump( $data['email']);
-        //dd($request);
 
-        // Validate the form data
-        $this->validate($request, [
-            'email'   => 'required|email',
-            'password' => 'required'
-        ]);
-
-        // Attempt to log the user in
-        echo $request->email. "</br>";
-        echo $request->password . "</br>";
-        $data=DB::table('users')->where("email",$request->email)->first();
-        echo $data->email. "</br>";
-        echo $data->password . "</br>";
-        if (Auth::guard('users')->attempt(['email' => $request->email, 'password' => $request->password],$request->remember)) {
-            // if successful, then redirect to their intended location
-            return redirect()->intended(route('user.homepage'));
-        }
-        if($data->email == $request->email && $data->password == $request->password){
-            return redirect()->intended(route('user.homepage'));
-        }
-        return "Failed";
-        // if unsuccessful, then redirect back to the login with the form data
-        return redirect()->back()->withInput($request->only('email', 'remember'));
-    }
-    public function show_signup()
-    {
-        return view('signup');
-    }
     public function signup(Request $request)
     {
         $request->validate([
@@ -80,22 +41,7 @@ class WelcomeController extends Controller{
             return redirect()->back()->withInput($request->only('email', 'name'));
           }
     }
-    public function create(array $data)
-    {
-      return User::create([
-        'name' => $data['name'],
-        'email' => $data['email'],
-        'password' => Hash::make($data['password'])
-      ]);
-    }
-    public function show_forgot_password()
-    {
-        return view('forgot_password');
-    }
-    public function forgot_password()
-    {
 
-    }
     public function show_questions()
     {
         return view('questions');
@@ -112,10 +58,7 @@ class WelcomeController extends Controller{
     {
         return view('friends');
     }
-    public function logout()
-    {
-        # code...
-    }
+
     public function show_notification()
     {
         return view('notification');
--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/','WelcomeController@welcome')->name('user.welcome');
 
-//Auth::routes();
-Route::get('/login','WelcomeController@show_login')->name('user.showlogin');
-Route::post('/login','WelcomeController@login')->name('user.login');
-Route::get('/signup','WelcomeController@show_signup')->name('user.showsignup');
-Route::post('/signup','WelcomeController@signup')->name('user.signup');
-Route::get('/forgot-password','WelcomeController@show_forgot_password')->name('user.showforgotpassword');
-Route::post('/forgot-password','WelcomeController@forgot_password')->name('user.forgotpassword');
+Auth::routes();
+Route::get('/login','Auth\AuthController@show_login')->name('user.showlogin');
+Route::post('/login','Auth\AuthController@login')->name('user.login');
+Route::get('/signup','Auth\AuthController@show_signup')->name('user.showsignup');
+Route::post('/signup','Auth\AuthController@signup')->name('user.signup');
+Route::get('/forgot-password','Auth\AuthController@show_forgot_password')->name('user.showforgotpassword');
+Route::post('/forgot-password','Auth\AuthController@forgot_password')->name('user.forgotpassword');
 Route::get('/homepage','WelcomeController@show_home')->name('user.homepage');
 Route::get('/questions','WelcomeController@show_questions')->name('user.questions');
 Route::get('/profile','WelcomeController@show_profile')->name('user.profile');
 Route::get('/friends','WelcomeController@show_friends')->name('user.friends');
 Route::get('/settings','WelcomeController@show_settings')->name('user.settings');
 Route::get('/notification','WelcomeController@show_notification')->name('user.notification');
-Route::get('/logout','WelcomeController@logout')->name('user.logout');
+Route::get('/logout','Auth\AuthController@logout')->name('user.logout');
 
 
 
